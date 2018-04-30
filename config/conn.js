4a09d1b39d3181629466e132e61a7c0716be3826
geina corrente <33171782+geina17@users.noreply.github.com>
2018-04-30 12:00:38 -0700

    Add files via upload

--- config/conn.js
+++ config/conn.js
@@ -1,3 +1,21 @@
 // * Inside the `connection.js` file, setup the code to connect Node to MySQL.
 
 // * Export the connection.
+//node depen
+var dolphin = require('mysql');
+var conn;
+
+//for heroku deploy
+if (process.env.JAWSBD_URL) {
+    conn = dolphin.createConnection(process.env.JAWSBD_URL);
+} else {
+    conn = dolphin.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'baked'
+    });
+}
+
+//export the connection
+module.exports = conn;
--- config/orm.js
+++ config/orm.js
@@ -7,3 +7,73 @@
 //      * `updateOne()`
 
 //    * Export the ORM object in `module.exports`.
+
+//import node depen
+var conn = require('./conn.js');
+
+//connect to mysql
+conn.connect(function (err) {
+    if (err) {
+        console.error('error connecting:' + err.stack);
+        return;
+    }
+    console.log('connected as id' + conn.threadId);
+});
+function objToSql(ob) {
+    // column1=value, column2=value2,...
+    var arr = [];
+   
+    for (var key in ob) {
+      arr.push(key + '=' + ob[key]);
+    }
+   
+    return arr.toString();
+   }
+//method for mysql
+var orm = {
+    //selectAll
+    selectAll: function (table, callback) {
+        //run mysql
+        conn.query("select * from " + table + ";", function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    //insertONe
+    insertOne: function (table, cols, vals, callback) {
+        
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += "?, ?";
+        queryString += ") ";
+        //run mysql
+        conn.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    //updateOne
+    updateOne: function (table, colVals, condition, callback) {
+        //run mysql
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(colVals);
+        queryString += " WHERE ";
+        queryString += condition;
+        //UODATE cakes SET eaten = true WHERE id = 1
+        conn.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    }
+};
+
+//export the orms
+module.exports = orm;
