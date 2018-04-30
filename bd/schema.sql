6d1924f613a3ee6eb193d46f484ba786f8df5617
geina corrente <33171782+geina17@users.noreply.github.com>
2018-04-30 12:00:11 -0700

    Add files via upload

--- bd/schema.sql
+++ bd/schema.sql
@@ -1,9 +1,9 @@
-create database baked_db;
-use baked_db;
+create database baked;
+use baked;
 
 CREATE TABLE cakes (
 	id int NOT NULL AUTO_INCREMENT,
 	cake_name varchar(50) NOT NULL,
-	devoured BOOLEAN NOT NULL,
+	eatin BOOLEAN NOT NULL,
 	PRIMARY KEY (id)
-);
+)
--- bd/seeds.sql
+++ bd/seeds.sql
@@ -1,7 +1,8 @@
-select * from baked_db.cakes;
-INSERT INTO cakes (cake,eatin) VALUES ('Lavender Earl Grey Teacake cupcake',false);
-INSERT INTO cakes (cake,eatin) VALUES ('Vanilla Butterfly Surprise cupcake',false);
-INSERT INTO cakes (cake,eatin) VALUES ('Cherry Blossom cupcake',false);
-INSERT INTO cakes (cake,eatin) VALUES ('Marble Brownie Fudge Cheesecake cupcake ',false);
-INSERT INTO cakes (cake,eatin) VALUES ('Peanut Butter Fudge cupcake',false);
-INSERT INTO cakes (cake,eatin) VALUES ('Lemon Elderflower “English Rose” cupcake',false);
+
+select * from baked.cakes;
+INSERT INTO cakes (cake_name,eatin) VALUES ('Lavender Earl Grey Teacake cupcake',false);
+INSERT INTO cakes (cake_name,eatin) VALUES ('Vanilla Butterfly Surprise cupcake',false);
+INSERT INTO cakes (cake_name,eatin) VALUES ('Cherry Blossom cupcake',false);
+INSERT INTO cakes (cake_name,eatin) VALUES ('Marble Brownie Fudge Cheesecake cupcake ',false);
+INSERT INTO cakes (cake_name,eatin) VALUES ('Peanut Butter Fudge cupcake',false);
+INSERT INTO cakes (cake_name,eatin) VALUES ('Lemon Elderflower “English Rose” cupcake',false);
